1978d5f493ac3e1fd18f63687c64a21eb61e9ee3
Nur A Alam <nuraalam376@gmail.com>
2020-10-03 08:27:59 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Node.js is working");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

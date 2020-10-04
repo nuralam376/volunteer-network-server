94dfa75db06e2633971220a251371877c8dc9ba7
Nur A Alam <nuraalam376@gmail.com>
2020-10-04 07:51:09 +0600

    Mongodb added

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8cle8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log("Database Error", err);
+    return;
+  }
+  console.log("Database connected");
+  app.get("/", (req, res) => {
+    res.send("Node.js is running");
+  });
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,9 +26,5 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("Node.js is working");
-});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

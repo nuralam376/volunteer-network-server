a3be6274cd9ff91e0a7371670cb9fa9464a83a7e
Nur A Alam <nuraalam376@gmail.com>
2020-10-04 18:42:33 +0600

    Deleted registered event

--- routes/volunteerRoutes.js
+++ routes/volunteerRoutes.js
@@ -1,3 +1,5 @@
+const ObjectId = require("mongodb").ObjectID;
+
 module.exports = (app, volunteersCollection) => {
   // Saves the registered event
   app.post("/volunteer/registration", (req, res) => {
@@ -34,5 +36,18 @@ module.exports = (app, volunteersCollection) => {
       }
       res.status(404).send("No Data found");
     });
+  });
+
+  // Deletes the registeres event
+  app.delete("/volunteer/event/delete", (req, res) => {
+    volunteersCollection
+      .deleteOne({ _id: ObjectId(req.body.id) })
+      .then((result) => {
+        if (result.deletedCount) {
+          return res.status(200).send(true);
+        }
+        return res.status(404).send(false);
+      })
+      .catch(() => res.status(404).send(false));
   });
 };
